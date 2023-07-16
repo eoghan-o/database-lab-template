30127aec7fd45364d21062c2d6a3bc43b7b79a0a
Hogchops <31632064+Hogchops@users.noreply.github.com>
2023-07-16 15:46:56 -0400

    Create Inventory1.py

---
+++ Inventory1.py
@@ -0,0 +1,11 @@
+def main():
+    max_list_size = 0   # get the user to input
+    file_name = ""      # both of these
+    
+    database = []       # the database
+    read_inventory_file(file_name, database, ...)
+    
+def read_inventory_file(file_name, database, ...):
+    # more code would follow...
+    
+main()
